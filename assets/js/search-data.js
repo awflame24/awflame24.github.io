ca53b070a884f162b6bb4ba8dcba6a3879ee1b37
awflame24 <awflame24@snu.ac.kr>
2025-05-06 06:09:14 +0000

    Deploying to gh-pages from @ awflame24/awflame24.github.io@60d0eaa0799234247a9363f21825981b3c3eb325 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -72,7 +72,29 @@ ninja.data = [{
               handler: () => {
                 window.location.href = "/blog/";
               },
-            },{id: "post-my-first-jekyll-blog-post",
+            },{id: "post-대한민국-자연명소",
+        
+          title: "대한민국 자연명소",
+        
+        description: "",
+        section: "Posts",
+        handler: () => {
+          
+            window.location.href = "/blog/2025/natural-attraction/";
+          
+        },
+      },{id: "post-markdown-문법-기초",
+        
+          title: "Markdown 문법 기초",
+        
+        description: "",
+        section: "Posts",
+        handler: () => {
+          
+            window.location.href = "/blog/2025/markdown/";
+          
+        },
+      },{id: "post-my-first-jekyll-blog-post",
         
           title: "My first Jekyll blog post",
         
