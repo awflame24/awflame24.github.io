59c2205c55ecedff19baecbc8c38f3dbce3eaccc
awflame24 <awflame24@snu.ac.kr>
2025-05-06 06:10:56 +0000

    Deploying to gh-pages from @ awflame24/awflame24.github.io@e09d3052685adc1198c883e51fded6e0a7999556 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -72,18 +72,7 @@ ninja.data = [{
               handler: () => {
                 window.location.href = "/blog/";
               },
-            },{id: "post-대한민국-자연명소",
-        
-          title: "대한민국 자연명소",
-        
-        description: "",
-        section: "Posts",
-        handler: () => {
-          
-            window.location.href = "/blog/2025/natural-attraction/";
-          
-        },
-      },{id: "post-markdown-문법-기초",
+            },{id: "post-markdown-문법-기초",
         
           title: "Markdown 문법 기초",
         
